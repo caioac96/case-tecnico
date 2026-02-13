18ccb3d93be75b9457b073f874881577891a0f90
Caio Carvalho <caaiosin@gmail.com>
2026-02-13 15:54:09 -0300

    [hotfix] ajuste CORS para render front

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://case-tecnico-1.onrender.com"],
         credentials: true,
     })
 );
