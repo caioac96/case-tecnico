255ca82ad3a8111f72eee4e4916bd2ee80e6af90
Caio Carvalho <caaiosin@gmail.com>
2026-02-13 15:47:44 -0300

    [hotfix] ajuste env

--- apps/frontend/src/api/backend.ts
+++ apps/frontend/src/api/backend.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
     timeout: 60000,
     withCredentials: true,
     headers: {
